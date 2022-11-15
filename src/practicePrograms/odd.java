2bc3f190998a61b82539191169c45055c98cdfba
megunasekaran <megunasekaran@gmail.com>
2022-11-15 15:21:21 +0530

    intial commit

--- src/practicePrograms/odd.java
+++ src/practicePrograms/odd.java
@@ -15,7 +15,7 @@ public class odd {
 			if (n % 2 == 0) {
 				System.out.println(n + "is an even number");
 			} else {
-				System.out.println(n + "is an odd number");
+				System.out.println(n + "is a --odd number");
 
 			}
 		}
