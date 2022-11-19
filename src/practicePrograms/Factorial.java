0d55b62aaf8fcd48be7808cad0952bd27f0303d2
megunasekaran <megunasekaran@gmail.com>
2022-11-19 12:01:36 +0530

    Factorial

---
+++ src/practicePrograms/Factorial.java
@@ -0,0 +1,18 @@
+package practicePrograms;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+
+		
+		int number = 5;
+		int fact =1;
+		
+		for (int i=0; i<=number; i++)
+		{
+			fact = fact*i;
+		}
+		System.out.println("Factorial for" + " " + number + " " + "is " + fact );
+	}
+
+}
