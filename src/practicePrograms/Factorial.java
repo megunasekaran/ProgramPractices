b6b2c9eaf85b8afee5978c33ad07a13ad6b5499e
megunasekaran <megunasekaran@gmail.com>
2022-11-26 11:47:54 +0530

    test

--- src/practicePrograms/Factorial.java
+++ src/practicePrograms/Factorial.java
@@ -3,6 +3,18 @@ package practicePrograms;
 public class Factorial {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
+
+		
+		int number = 5;
+		int fact =1;
+		
+		for (int i=0; i<=number; i++)
+		{
+			fact = fact*i;
+		}
+		System.out.println("Factorial for" + " " + number + " " + "is " + fact );
+=======
 		
 		int number = 5;
 		int fact = 1;
@@ -14,6 +26,7 @@ public class Factorial {
 		
 		System.out.println("Factorial for the" + number + " " + "is" +" " + fact);
 
+>>>>>>> refs/remotes/origin/dev
 	}
 
 }
